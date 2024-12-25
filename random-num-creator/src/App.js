478307a36d9c899dc24e3b4ce9b2b5dd2a799077
Gulsahyldrm <yildirimgulsah2323@gmail.com>
2024-12-25 16:13:45 +0300

    random-num-creator added

---
+++ random-num-creator/src/App.js
@@ -0,0 +1,12 @@
+import Creator from './components/Creator';
+function App() {
+  return (
+    <div className="App">
+      <Creator>
+
+      </Creator>
+    </div>
+  );
+}
+
+export default App;
